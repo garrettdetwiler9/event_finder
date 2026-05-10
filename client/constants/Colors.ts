7c0b5a4948986cb2a89076b42c340c91bd53e3ec
Ethan Nguyen <etanguyen@ucdavis.edu>
2026-05-10 00:46:32 -0700

    Fixed color literal errors to comply with linter

--- client/constants/Colors.ts
+++ client/constants/Colors.ts
@@ -1,5 +1,11 @@
 export const Colors = {
   primary: '#9333ea',
   secondary: '#ec4899',
-  indigo: '#4f46e5',
+  brand: '#4f46e5',
+  white: '#fff',
+  textPrimary: '#111827',
+  textSecondary: '#6b7280',
+  textPlaceholder: '#9ca3af',
+  border: '#e5e7eb',
+  error: '#ef4444',
 };
